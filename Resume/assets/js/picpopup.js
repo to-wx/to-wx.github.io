9afa58009d4e74c515e59d672ce4026be334bd77
yyr <to_wx@outlook.com>
2019-10-31 00:39:20 +0800

    Site updated: 2019-10-31 00:39:20

---
+++ Resume/assets/js/picpopup.js
@@ -0,0 +1,9 @@
+
+
+
+  // function initPics(num){
+            //     var s = "<a href="\"images/fulls/07.jpg\"" hidden="\"hidden\"">\n" +
+            //     "\t\t\t\t\t\t\t\t\t\t<img src="\"images/thumbs/7.jpg\"" alt="\"\"/">\n" +
+            //     "\t\t\t\t\t\t\t\t\t\t<h3 hidden="\"hidden\""></h3>\n" +
+            //     "                                    </a>";
+            //     for(var i=0; i
